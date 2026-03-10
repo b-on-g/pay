bb26b60b57739ddb2f8fd4cca2c787c922505323
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-10 07:40:02 +0000

    Deploying to gh-pages from @ b-on-g/pay@98a7dea4c76d9967ce5e140800e23c5c0fea8ecc 🚀

--- node.d.ts
+++ node.d.ts
@@ -4698,8 +4698,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_crypto_salt(): Uint8Array<ArrayBuffer>;
-    const $mol_crypto_salt_once: Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_nonce(): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_salt: typeof $mol_crypto2_nonce;
 }
 
 declare namespace $ {
@@ -4959,7 +4962,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_crypto_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_hash: typeof $mol_crypto2_hash;
 }
 
 declare namespace $ {
